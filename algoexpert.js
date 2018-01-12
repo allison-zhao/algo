93729590c9e786dbb6ac4c1b0db3e065d2254952
Allison von Bargen <gryffinzhao@gmail.com>
2018-01-12 10:24:34 -0500

    added find closest value in bst

--- algoexpert.js
+++ algoexpert.js
@@ -11,3 +11,26 @@ function twoNumberSum(array, targetSum) {
   }
   return [];
 }
+
+//Find Closest Value In BST
+
+function findClosestValueInBst(tree, target) {
+  if (tree === null) return null;
+  let closest = tree.value;
+
+  while (tree !== null) {
+    if (tree.value === target) return tree.value;
+    let difference = Math.abs(tree.value - target);
+    if (difference < Math.abs(closest - target)) {
+      closest = tree.value;
+    }
+
+    if (tree.value < target) {
+      tree = tree.right;
+    } else {
+      tree = tree.left;
+    }
+  }
+
+  return closest;
+}
