f81ebcda113d103b436a6d728b3a148de6634f5a
Allison von Bargen <gryffinzhao@gmail.com>
2018-01-25 13:56:44 -0500

    added sell stock problem

--- misc.js
+++ misc.js
@@ -8,7 +8,7 @@ function transformString(str) {
   let arr = str.split('');
   let counter = 1;
   let curChar = arr[0];
-  let result = []
+  let result = [];
 
   for (let i = 1; i <= arr.length; i++) {
     if (arr[i] === curChar) {
@@ -17,10 +17,29 @@ function transformString(str) {
       let text = curChar + counter;
       result.push(text);
       counter = 1;
-      curChar = arr[i]
+      curChar = arr[i];
     }
   }
 
   return result.join('');
 }
 
+//Sell Stock problem
+
+function getMaxProfit(stockPrices) {
+  if (stockPrices.length < 2) {
+    throw new Error('Need to have at least 2 prices to calculate profit');
+  }
+
+  let minPrice = stockPrices[0];
+  let maxProfit = stockPrices[1] - stockPrices[0];
+
+  for (let i = 1; i < stockPrices.length; i++) {
+    let currentPrice = stockPrices[i];
+    let currentProfit = stockPrices[i] - minPrice
+    maxProfit = Math.max(maxProfit, currentProfit);
+    minPrice = Math.min(minPrice, currentPrice);
+  }
+
+  return maxProfit;
+}
